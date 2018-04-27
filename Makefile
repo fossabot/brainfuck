eb581462d70c7778be9facc6d34c4f38dfd2c151
Sam Yong <yongmc.sam2014@outlook.com>
2018-04-27 15:24:49 -0400

    handle macros

--- Makefile
+++ Makefile
@@ -1,8 +1,15 @@
-.PHONY: clean, manual
+.PHONY: clean, manual, compile
 
 clean:
 	rm -rf __pycache__
 
-manual: brainfuck.py lexer.py
+manual: brainfuck.py lexer.py macros.py
 	@python3
+	@make clean
+
+compile: brainfuck.py lexer.py macros.py
+	@python3 brainfuck.py
+	@python3 lexer.py
+	@python3 macros.py
+	# Compilation passed in Python 3.
 	@make clean
--- macros.py
+++ macros.py
@@ -13,6 +13,40 @@ def __find_macros (T):
     M = [macro [1:] for macro in M_] # remove definition command '$'
     return (T_, M)
 
+def __open_file (name):
+    # name is string without '.bf' suffix
+    file_path = name.strip () + '.bf'
+    try:
+        f = open (file_path, 'r')
+    except:
+        raise ValueError ('Brainfuck: macro definition does not exist.')
+    L = [line for line in f] # parse file
+    T = tokenize (L)
+    f.close ()
+    return T
+
 def __substitute_once (T, M):
     # T is token list (without macro definitions)
     # M is macro definitions of T
+    M = list (set (M)) # remove duplicate definitions
+    MACROS = {} # M definition as key, token list as value
+    for macro in M:
+        macro_tokens = __open_file (macro)
+        MACROS [macro] = macro_tokens
+    for A in MACROS:
+        for B in MACROS: # pairwise loop
+            if A != B: # distinct items
+                if (A in MACROS [B]) and (B in MACROS [A]):
+                    # mutual recursive definition - not allow!
+                    raise ValueError ('Brainfuck: mutually recursive macro definition in {_A} and {_B}'.format (_A = A, _B = B))
+    S = [] # substituted new token list
+    for token in T:
+        if token [0] in __built_in: # a regular built-in command
+            S.append (token)
+        else:
+            if token not in MACROS: # definition non-exist
+                raise ValueError ('Brainfuck: cannot find macro definition of {_def}'.format (_def = token))
+            for _token in MACROS [token]: # retrieve macro definition token list
+                S.append (_token)
+    return S
+
