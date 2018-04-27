6dfc1d4437ff4d64116c76129d34dd66490c16a2
Sam Yong <yongmc.sam2014@outlook.com>
2018-04-27 14:41:34 -0400

    lexer

---
+++ lexer.py
@@ -0,0 +1,37 @@
+# Brainfuck interpret - Lexer
+
+__built_in = ['+', '-', '>', '<', '.', ',', '[', ']']
+
+def __remove_comments (L):
+    # L is string list
+    # truncates strings in L where comments start
+    for i in range (len (L)):
+        if '#' in L[i]:
+            comment_start = L[i].find ('#') # find first occurance of '#'
+            L[i] = L[i] [:comment_start] # slice string before comment_start
+    return L
+
+def __remove_whitespaces (L):
+    # L is string list (without comments)
+    # removes empty lines & whitespaces around lines
+    L = [line.strip () for line in L] # remove whitespaces around
+    L = [line for line in L if len (line) > 0] # remove empty lines
+    return L
+
+def tokenize (L):
+    # L is string list (without comments & empty lines)
+    # LL is string list list (without whitespaces)
+    LL = [[token.strip () for token in line.split (' ')] for line in L]
+    T = []
+    for line in LL:
+        for token in line:
+            # check if token is string of built-in commands
+            if token [0] in __built_in:
+                # add individual chars into token list
+                for c in token:
+                    T.append (c)
+            else:
+                # add entire token into token list
+                T.append (token)
+    return T
+
